b9fdd4a26cbce875765a24bcd9319fe3139714b6
Yuri Cherkasov <straight@MacBook-Air-MacAir.local>
2014-01-18 23:31:10 +0400

    x

--- cmake/TBBDependency.cmake
+++
@@ -1,9 +0,0 @@
-
-macro(TBBDependency)
-  find_package(TBB REQUIRED)
-  include_directories(${TBB_INCLUDE_DIRS})
-endmacro(TBBDependency)
-
-function(TBBDependencyLink TARGET)
-  target_link_libraries(${TARGET} ${TBB_LIBRARIES})
-endfunction(TBBDependencyLink)
